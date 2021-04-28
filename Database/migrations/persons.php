7c3cb6fdeae3fb5679043c40fe55cf413ce08502
ali-hammam <ali.hammam04@gmail.com>
2021-04-28 19:07:29 +0200

    Change project structure.

---
+++ Database/migrations/persons.php
@@ -0,0 +1,32 @@
+<?php
+namespace Database\migrations;
+use DB\Table\Facades\ColumnPropertyFacade;
+use DB\Table\Facades\Table;
+use DB\Table\Migrations\migration;
+
+require_once('Nesc/DB/Table/Migrations/migration.php');
+require_once('Nesc/DB/Table/Facades/ColumnPropertyFacade.php');
+require_once('Nesc/DB/Table/Facades/Table.php');
+
+class persons extends migration
+{
+    public function up(){
+        Table::create($this->className , function (){
+            $id = ColumnPropertyFacade::SetColumnBase('Id')
+                ->Number()
+                ->primaryKe y()
+                ->getColumnProperty();
+
+            $firstName = ColumnPropertyFacade::SetColumnBase('FirstName')
+                ->String(10)
+                ->getColumnProperty();
+
+            return [$id, $firstName];
+        });
+    }
+
+    public function down(){
+        Table::drop($this->className);
+    }
+
+}
